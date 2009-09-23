0f74c89dd1c9023c7ea23f086a92349534bf2905
barkyoungjun <Goyange@cat-studio.pe.kr>
2009-09-23 19:19:09 +0900

    main화면 세분화 작업

--- main.py
+++ main.py
@@ -4,15 +4,28 @@ import pygame, sys,os
 from pygame.locals import * 
 import app
 
-pygame.init() 
-inputimg = app.load_image("player.bmp", -1)
-window = pygame.display.set_mode((460, 320)) 
-pygame.display.set_caption('Rabit Hazard') 
-screen = pygame.display.get_surface() 
-screen.blit(inputimg, (0,0))
-pygame.display.flip()
+
+def main():
+    # Start up pygame/make screen
+    pygame.init() 
+    window = pygame.display.set_mode((460, 320)) 
+    screen = pygame.display.get_surface() 
+    pygame.display.set_caption('Rabit Hazard') 
+    # make background
+    background = app.load_image("player.bmp", -1)
+    # blit background on to the screen
+    screen.blit(background, (0,0))
+    pygame.display.flip()
+
+    # load fonts
+    
+    # make the text needed
 
 
+
+if __name__ == '__main__':
+    main()
+ 
 while True: 
    app.input(pygame.event.get()) 
- 
+
